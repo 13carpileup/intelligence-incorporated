77192f0fa7573ae91984096e9e9b71e1c39afcd8
13carpileup <climie.alex@gmail.com>
2024-07-06 21:35:20 +0700

    game start

--- globals.js
+++ globals.js
@@ -1,2 +1,4 @@
 var governmentOn = 0;
 var competitionOn = 0;
+var money = 0;
+var moneyPS = 0;
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var projects = [];
 
 function displayUpdate() {
+    // updates projects
     for (var i = 0; i < projects.length; i++) {
         if (projects[i].purchased || !projects[i].active) {
             document.getElementById(projects[i].id).style.display = "none";
-            console.log("BRUH");
         }
 
         else if (projects[i].active) {
@@ -12,6 +12,7 @@ function displayUpdate() {
         }
     }
 
+    // updates main 3 sections in 1st part of game
     if (governmentOn == 0){
         document.getElementById("government").style.display = "none";
     } 
@@ -24,7 +25,11 @@ function displayUpdate() {
     } 
     else {
         document.getElementById("competition").style.display = "";    
-    }    
+    }
+
+    // updates money
+    money += moneyPS;
+    document.getElementById("moneyDisplay").innerHTML = Math.floor(money*100)/100;
 }
 
 function displayProjects(project) {
@@ -38,20 +43,43 @@ function displayProjects(project) {
     element.appendChild(newProject, element.firstChild);
 }
 
+function findProject(id) {
+    console.log(projects.filter(project => project.id===id)[0].id);
+    for (var i = 0; i < projects.length; i++) {
+        if (projects[i].id==id) return i;
+    }
+}
+
+var project0 = {
+    id: "p0",
+    title: "Hire your first developer",
+    description: "start your adventure",
+    effect: function() {
+        projects[findProject("p1")].active = true;
+        moneyPS = 0.01
+
+        this.purchased = true;
+    },
+    purchased: false,
+    active: true,
+}
+
+projects.push(project0);
+
+
 var project1 = {
     id: "p1",
     title: "Leak r&d secrets",
     description: "gain two gabillion dollars",
     effect: function() {
         competitionOn = 1;
+        projects[findProject("p2")].active = true;
+        money += 20000000;
+
         this.purchased = true;
-        projects[1].active = true;
-
-        displayUpdate();
-
     },
     purchased: false,
-    active: true,
+    active: false,
 }
 
 projects.push(project1);
@@ -63,9 +91,9 @@ var project2 = {
     description: "gain three gabillion dollars",
     effect: function() {
         governmentOn = 1;
-        this.purchased = true;
+        money += 300000000;
 
-        displayUpdate();
+        this.purchased = true;
     },
     purchased: false,
     active: false,
@@ -77,5 +105,9 @@ for (var i = 0; i < projects.length; i++) {
     displayProjects(projects[i]);
 }
 
-displayUpdate()
+
+
+window.setInterval(function(){
+    displayUpdate();
+})
 
