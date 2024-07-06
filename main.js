ee9be9f8f70255bc370a730a321be7ca0ff6ccd4
13carpileup <climie.alex@gmail.com>
2024-07-06 20:13:30 +0700

    improved project system

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
 var projects = [];
 
 function displayUpdate() {
+    for (var i = 0; i < projects.length; i++) {
+        if (projects[i].purchased || !projects[i].active) {
+            document.getElementById(projects[i].id).style.display = "none";
+            console.log("BRUH");
+        }
+
+        else if (projects[i].active) {
+            document.getElementById(projects[i].id).style.display = "";
+        }
+    }
+
     if (governmentOn == 0){
-        document.getElementById("government").style.display="none";
-        } else {
-        document.getElementById("government").style.display="";    
+        document.getElementById("government").style.display = "none";
+    } 
+    else {
+        document.getElementById("government").style.display = "";    
     }   
     
     if (competitionOn == 0){
-        document.getElementById("competition").style.display="none";
-        } else {
-        document.getElementById("competition").style.display="";    
+        document.getElementById("competition").style.display = "none";
+    } 
+    else {
+        document.getElementById("competition").style.display = "";    
     }    
 }
 
 function displayProjects(project) {
     var newProject = document.createElement("button");
     newProject.setAttribute("id", project.id);
-    newProject.innerHTML = project.title;
+    newProject.innerHTML = `<p class = "projectTitle">${project.title}</p><p class = "projectDescription">${project.description}</p>`;
     newProject.onclick = function(){project.effect()};
     newProject.setAttribute("class", "projectButton");
 
@@ -28,11 +41,17 @@ function displayProjects(project) {
 var project1 = {
     id: "p1",
     title: "Leak r&d secrets",
+    description: "gain two gabillion dollars",
     effect: function() {
         competitionOn = 1;
+        this.purchased = true;
+        projects[1].active = true;
 
         displayUpdate();
-    }
+
+    },
+    purchased: false,
+    active: true,
 }
 
 projects.push(project1);
@@ -41,11 +60,15 @@ projects.push(project1);
 var project2 = {
     id: "p2",
     title: "Commit a crime",
+    description: "gain three gabillion dollars",
     effect: function() {
         governmentOn = 1;
+        this.purchased = true;
 
         displayUpdate();
-    }
+    },
+    purchased: false,
+    active: false,
 }
 
 projects.push(project2);
